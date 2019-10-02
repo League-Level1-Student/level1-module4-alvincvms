dd45f6e37ad04139dcee9d84b6b5c82845566d38
League Student <league@iMac-21.attlocal.net>
2019-10-01 18:29:47 -0700

    derp
    null

--- src/_12_slot_machine/SlotMachine.java
+++ src/_12_slot_machine/SlotMachine.java
@@ -11,20 +11,21 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class SlotMachine implements ActionListener {
-	JLabel icon1 = new JLabel();
-	JLabel icon2 = new JLabel();
-	JLabel icon3 = new JLabel();
+	JLabel icon1; JLabel a = loadImage("cherry.png"); JLabel d = loadImage("ace.png");
+	JLabel icon2; JLabel b = loadImage("orange.png"); JLabel e = loadImage("7.png");
+	JLabel icon3; JLabel c = loadImage("grape.png");
 	JPanel panel = new JPanel();
-	JFrame frame = new JFrame();
+	JFrame frame = new JFrame("Slot Machine");
 	JButton button = new JButton();
 	
 	void run() {
 		button.setText("SPIN");
 		panel.add(button);
-		icon1 = loadImage("7.png"); 
+		icon1 = loadImage("7.png");
 		icon2 = loadImage("7.png");
 		icon3 = loadImage("7.png");
 		panel.add(icon1); panel.add(icon2); panel.add(icon3);
@@ -44,27 +45,44 @@ public class SlotMachine implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		System.out.println("clicked");
+		panel.remove(icon1);
+		icon1 = randomImage();
+		panel.add(icon1);
 		
+		panel.remove(icon2);
+		icon2 = randomImage();
+		panel.add(icon2);
+		
+		panel.remove(icon3);
+		icon3 = randomImage();
+		panel.add(icon3);
+		
+		frame.pack();
+		
+		if() { unfinished
+			JOptionPane.showMessageDialog( null , "Win!");
+		}
 	}
-	JLabel randomImage() {
+	private JLabel randomImage() {
 		int x = new Random().nextInt(31);
 		if(x <= 8) {
-			return loadImage("cherry.png");
+			return a;
 		}
 		else if(x > 8 && x <= 17) {
-			return loadImage("orange.png");
+			return b;
 		}
 		else if(x > 17 && x <= 26) {
-			return loadImage("grape.png");
+			return c;
 		}
 		else if(x > 26 && x <= 29) {
-			return loadImage("ace.png");
+			return d;
 		}
 		else {
-			return loadImage("7.png");
+			return e;
 		}
 	}
 	
 }
 
-unfinished
+
